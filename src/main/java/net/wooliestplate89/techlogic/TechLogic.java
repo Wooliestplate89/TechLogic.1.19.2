09dc8c18d1f645b46485810508ec12f996032ffc
Wooliestplate89 <michaeledwin789@gmail.com>
2022-12-09 22:04:18 +0800

    Removed an important comment.

--- src/main/java/net/wooliestplate89/techlogic/TechLogic.java
+++ src/main/java/net/wooliestplate89/techlogic/TechLogic.java
@@ -17,7 +17,7 @@ public class TechLogic {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "techlogic";
     private static final Logger LOGGER = LogUtils.getLogger();
-//Totally useful comment!
+
     public TechLogic() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
